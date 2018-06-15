cda8cda381c192b7a79b0494833b5e5dad02565a
dizdaroglu <furkndizdaroglu@gmail.com>
2018-06-15 12:31:10 +0300

    adding sparkline charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,23 +1,23 @@
 import  React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines,SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
  class WeatherList extends Component {
 
     renderWeather(cityData){
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         console.log(temps);
         
         return(
             <tr key={name}>
                 <td>{ name }</td>
-                <td>
-                    <Sparklines height={120} width={180} data={temps}>
-                        <SparklinesLine color="red"/>
-                    </Sparklines>
-                </td>
+                <td> <Chart color="orange" data={temps} units="K"/> </td>
+                <td> <Chart color="green" data={pressures} units="hPa"/> </td>
+                <td> <Chart color="black" data={humidities} units="%"/> </td>
             </tr>
         );
     }
@@ -28,9 +28,9 @@ import { Sparklines,SparklinesLine } from 'react-sparklines';
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
